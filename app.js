91ec87b052d442e2d9a83b17c89ac185b7532822
Gavin Feriancek <gferiancek@gmail.com>
2024-04-16 19:45:16 -0400

    WIP Making AJAX call to Giphy

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 console.log("Let's get this party started!");
+const BASE_URL = 'http://api.giphy.com/v1/gifs'
+const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+
+const $giphyForm = document.querySelector('#Giphy')
+const $giphyInput = document.querySelector('#Giphy-search')
+
+/** Sets Event Listener for form submission */
+function start() {
+  $giphyForm.addEventListener('submit', handleSubmission)
+}
+
+/** Gets a gif and appends to DOM */
+async function handleSubmission(evt) {
+
+  const gif = await getGif();
+  // Append to dom
+}
+
+/** Makes AJAX call to Giphy api to get gif data
+ *    - Return String of the gif url
+ */
+async function getGif() {
+  const search = $giphyInput.value;
+
+  const resp = await fetch(
+    `${BASE_URL}/search`,
+    {
+
+    }
+  )
+
+}
+export { start }
