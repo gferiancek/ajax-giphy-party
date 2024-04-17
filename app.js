eab242c2b7f7c6dd708199944634baa6536f3adc
Anya Aven <leafeon96@gmail.com>
2024-04-16 20:11:19 -0400

    finished getGifUrl function to return the url

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
 console.log("Let's get this party started!");
-const BASE_URL = 'http://api.giphy.com/v1/gifs'
-const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+const BASE_URL = 'http://api.giphy.com/v1/gifs';
+const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 
-const $giphyForm = document.querySelector('#Giphy')
-const $giphyInput = document.querySelector('#Giphy-search')
+const $giphyForm = document.querySelector('#Giphy');
+const $giphyInput = document.querySelector('#Giphy-search');
 
 /** Sets Event Listener for form submission */
 function start() {
-  $giphyForm.addEventListener('submit', handleSubmission)
+  $giphyForm.addEventListener('submit', handleSubmission);
 }
 
 /** Gets a gif and appends to DOM */
 async function handleSubmission(evt) {
+  evt.preventDefault();
 
-  const gif = await getGif();
+  const gif = await getGifUrl();
   // Append to dom
 }
 
 /** Makes AJAX call to Giphy api to get gif data
  *    - Return String of the gif url
  */
-async function getGif() {
+async function getGifUrl() {
   const search = $giphyInput.value;
 
+  const params = new URLSearchParams({ q: search, api_key: API_KEY });
+
   const resp = await fetch(
-    `${BASE_URL}/search`,
-    {
+    `${BASE_URL}/search/?${params}`
+  );
+  const gifs = await resp.json();
 
-    }
-  )
+  const amtOfGifs = gifs.data.length;
 
+  const randomIndex = Math.floor(Math.random() * (amtOfGifs + 1));
+
+  console.log("gif's url:", {data: gifs.data[8].embed_url});
+  const gifsUrl = gifs.data[randomIndex].embed_url;
+
+  return gifsUrl;
 }
-export { start }
+export { start };
